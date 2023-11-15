1bd2bc35fc5f82f36f582936abdaebbd37fa3a14
KidWreck <139178012+KidWreck@users.noreply.github.com>
2023-11-15 23:19:37 +0200

    Update FUNC3.c

--- FUNC3.c
+++ FUNC3.c
@@ -40,3 +40,65 @@ void mod_(stack_t **st, unsigned int ln)
 	free((*st)->prev);
 	(*st)->prev = NULL;
 }
+
+/**
+ * my_node - Create node.
+ * @n: Number.
+ * Return: pointer or NULL.
+ */
+stack_t *my_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err_1(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Free nodes.
+ */
+void free_nodes(void)
+{
+	stack_t *t;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		t = head;
+		head = head->next;
+		free(t);
+	}
+}
+
+
+/**
+ * add_q - Add node queue.
+ * @node: new node.
+ * @ln: line number.
+ */
+void add_q(stack_t **node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *t;
+
+	if (node == NULL || *node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *node;
+		return;
+	}
+	t = head;
+	while (t->next != NULL)
+		t = t->next;
+
+	t->next = *node;
+	(*node)->prev = t;
+
+}
